d73d50435c90f06af01d4bb3e9e62b254a3622a6
Jovan <jovanv0803@gmail.com>
2022-07-26 16:08:39 +0200

    redux setup

--- src/components/Movies/Movies.jsx
+++ src/components/Movies/Movies.jsx
@@ -1,6 +1,13 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { Box, CircularProgress, useMediaQuery, Typography } from '@mui/material';
+import { useSelector } from 'react-redux';
+
+import { useGetMoviesQuery } from '../../services/TMDB';
 
 function Movies() {
+  const { data } = useGetMoviesQuery(); //fetching data from an API
+  console.log(data);
+
   return (
     <div>Movies</div>
   );
--- src/index.js
+++ src/index.js
@@ -3,17 +3,21 @@ import ReactDOM from 'react-dom/client'; //connects the app with public/index.ht
 
 import { BrowserRouter } from 'react-router-dom';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
+import { Provider } from 'react-redux';
 
 import App from './components/App';
+import store from './app/store';
 
 //with ReactDOM we connect our app to the root div inside public/index.html
 const root = ReactDOM.createRoot(document.getElementById('root'));
 const theme = createTheme({}); //for now we'll pass an empty object
 
 root.render(
-  <ThemeProvider theme={theme}>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </ThemeProvider>,
+  <Provider store={store}>
+    <ThemeProvider theme={theme}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </ThemeProvider>
+  </Provider>,
 );
